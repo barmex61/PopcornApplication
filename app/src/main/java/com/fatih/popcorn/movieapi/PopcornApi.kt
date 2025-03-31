b47e21585440ffdcaadf6dcfb8c9359381168586
Fatih <haunter61@hotmail.com>
2025-04-01 00:47:56 +0300

    Update PopcornApi.kt

--- app/src/main/java/com/fatih/popcorn/movieapi/PopcornApi.kt
+++ app/src/main/java/com/fatih/popcorn/movieapi/PopcornApi.kt
@@ -17,17 +17,7 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface PopcornApi {
-    //https://api.themoviedb.org/3/movie/505642/similar?api_key=ae624ef782f69d5092464dffa234178b&language=en-US&page=1
-    //https://api.themoviedb.org/3/movie/{movie_id}/recommendations?api_key=ae624ef782f69d5092464dffa234178b&language=en-US&page=1
-    //https://api.themoviedb.org/3/movie/505642?api_key=ae624ef782f69d5092464dffa234178b&language=en-US
-    //https://api.themoviedb.org/3/tv/100088/watch/providers?api_key=ae624ef782f69d5092464dffa234178b
-    //https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc%2Cc0KYU2j0TM4%2CeIho2S0ZahI&key=AIzaSyDjn7FjfG2kUgWP4D5-w1GoigWWyyT_ZQs
-    //https://api.themoviedb.org/3/discover/tv?api_key=ae624ef782f69d5092464dffa234178b&sort_by=popularity.desc&page=1
-    //https://api.themoviedb.org/3/search/movie?api_key=ae624ef782f69d5092464dffa234178b&query=Spi&page=1
-    //https://api.themoviedb.org/3/tv/85552/videos?api_key=ae624ef782f69d5092464dffa234178b
-    //https://api.themoviedb.org/3/movie/1037353/credits?api_key=ae624ef782f69d5092464dffa234178b&language=en-US
-    //https://api.themoviedb.org/3/discover/movie?api_key=ae624ef782f69d5092464dffa234178b&sort_by=popularity.desc&page=1&with_genres=80
-    //https://api.themoviedb.org/3/movie/505642/reviews?api_key=ae624ef782f69d5092464dffa234178b&language=en-US&page=1
+    
 
     @GET("{name}/{id}/videos")
     suspend fun getVideos(
